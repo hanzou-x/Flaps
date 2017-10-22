ec7feada5781cce9f38becd6df2b0a8dd4bd62d6
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-10-22 01:15:54 +0000

    Drop minimum fee value

--- src/main.cpp
+++ src/main.cpp
@@ -51,9 +51,9 @@ bool fTxIndex = false;
 unsigned int nCoinCacheSize = 5000;
 
 /** Fees smaller than this (in satoshi) are considered zero fee (for transaction creation) */
-int64 CTransaction::nMinTxFee = 11337;
+int64 CTransaction::nMinTxFee = 20;
 /** Fees smaller than this (in satoshi) are considered zero fee (for relaying) */
-int64 CTransaction::nMinRelayTxFee = 11337;
+int64 CTransaction::nMinRelayTxFee = 20;
 
 CMedianFilter<int> cPeerBlockCounts(8, 0); // Amount of blocks that other nodes claim to have
 
