7fa0b102c69e3d4efd500c0d2d3df9e1330f1a52
Packetsonpackets <36382620+Packetsonpackets@users.noreply.github.com>
2018-02-17 14:16:35 -0500

    Update version.h

--- src/version.h
+++ src/version.h
@@ -31,7 +31,7 @@ static const int PROTOCOL_VERSION = 5050000;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 4000000;
+static const int MIN_PEER_PROTO_VERSION = 5000000;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -39,7 +39,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 00001;
-static const int NOBLKS_VERSION_END = 4000000;
+static const int NOBLKS_VERSION_END = 5000000;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
